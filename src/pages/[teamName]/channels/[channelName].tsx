edafa70c86f38c4773acdeab1e020d50efdf44aa
Kevin Wang <kwangsan@gmail.com>
2020-07-11 20:33:38 -0400

    delete graphql fetching

--- src/pages/[teamName]/channels/[channelName].tsx
+++ src/pages/[teamName]/channels/[channelName].tsx
@@ -1,112 +1,35 @@
-import { Fragment, useReducer, useState, useEffect, FormEvent } from "react"
-import { NextPage, GetServerSideProps } from "next"
-import useSwr from "swr"
-import { GraphQLClient } from "graphql-request"
-import styled, { BaseProps } from "styled-components"
+import { useState } from "react"
+import { NextPage, GetStaticProps, GetStaticPaths } from "next"
+import styled from "styled-components"
 
 import { SlackLayout } from "components/SlackLayout"
-import { useAuth, useRightPanel } from "hooks"
+import {
+  useAuth,
+  // useRightPanel
+} from "hooks"
 
-const ENDPOINT = process.env.ENDPOINT as string
-const client = new GraphQLClient(ENDPOINT)
-
-const ROW_FRAGMENT = `
-fragment Row on Row {
-  PK
-  SK
-  createdAt
-  updatedAt
-}
-`
-
-const GET_USER = `
-${ROW_FRAGMENT}
-query GetUser($username: String!, $email: String!) {
-  getUser(username: $username, email: $email) {
-    ...Row
-  firstName
-  lastName
-  username
-  email
-  avatarUrl
-  }
-}
-`
-
-const QUERY_MESSAGES_QUERY = `
-${ROW_FRAGMENT}
-query QueryMessages($teamName: String!, $channelName: String!) {
-  queryMessages(teamName: $teamName, channelName: $channelName) {
-    ...Row
-    body
-    username
-  }
-}
-`
-
-const CREATE_MESSAGE_MUTATION = `
-${ROW_FRAGMENT}
-mutation CreateMessage(
-  $teamName: String!
-  $channelName: String!
-  $username: String!
-  $body: String!
-) {
-  createMessage(
-    teamName: $teamName
-    channelName: $channelName
-    username: $username
-    body: $body
-  ) {
-    ...Row
-    body
-  }
-}
-`
-interface User {
-  PK: string
-  SK: string
-  username?: string
-  firstName?: string
-  lastName?: string
-  email?: string
-  createdAt?: string
-  updatedAt?: string
-  avatarUrl?: string
-}
-
-interface Message {
-  PK: string
-  SK: string
-  createdAt: string
-  updatedAt?: string
-  body: string
-  teamName: string
-  channelName: string
-  username: string
-}
-const Sticky = styled.div`
-  position: sticky;
-  top: 1.5rem;
-  transform: translateY(-50%);
-`
-const MessageListDayDivider = styled.div`
-  height: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-const Day = styled.small`
-  font-weight: 500;
-  position: absolute;
-  border: 1px solid ${(p: BaseProps) => p.theme.muted};
-  border-radius: 24px;
-  height: 28px;
-  line-height: 27px;
-  padding: 0 16px;
-  background: ${(p: BaseProps) => p.theme.background};
-  z-index: 5;
-`
+// const Sticky = styled.div`
+//   position: sticky;
+//   top: 1.5rem;
+//   transform: translateY(-50%);
+// `
+// const MessageListDayDivider = styled.div`
+//   height: 1rem;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `
+// const Day = styled.small`
+//   font-weight: 500;
+//   position: absolute;
+//   border: 1px solid ${(p: BaseProps) => p.theme.muted};
+//   border-radius: 24px;
+//   height: 28px;
+//   line-height: 27px;
+//   padding: 0 16px;
+//   background: ${(p: BaseProps) => p.theme.background};
+//   z-index: 5;
+// `
 
 const InputContainer = styled.div`
   position: absolute;
@@ -124,181 +47,42 @@ const InputContainer = styled.div`
   }
 `
 
-enum UserActionType {
-  SET_USER = "SET_USER",
-}
-type UserAction = {
-  type: UserActionType
-  data: User
-  username: string
-}
-const usersReducer = (
-  state: { [username: string]: User },
-  action: UserAction
-) => {
-  switch (action.type) {
-    case UserActionType.SET_USER:
-      return { ...state, [action.username]: action.data }
-    default:
-      return state
-  }
-}
-const DateGroup = styled.div`
-  border-bottom: 1px solid ${(p: BaseProps) => p.theme.muted};
-  padding-bottom: 1rem;
-`
-const Message = styled.div`
-  display: grid;
-  grid-template-columns: 48px auto;
-  padding: 8px 20px;
+// const DateGroup = styled.div`
+//   border-bottom: 1px solid ${(p: BaseProps) => p.theme.muted};
+//   padding-bottom: 1rem;
+// `
+// const Message = styled.div`
+//   display: grid;
+//   grid-template-columns: 48px auto;
+//   padding: 8px 20px;
 
-  transition: background 100ms ease-in-out;
-  :hover {
-    background: ${(p: BaseProps) => p.theme.muted};
-  }
-`
-const Img = styled.img`
-  height: 40px;
-  width: 40px;
-`
+//   transition: background 100ms ease-in-out;
+//   :hover {
+//     background: ${(p: BaseProps) => p.theme.muted};
+//   }
+// `
+// const Img = styled.img`
+//   height: 40px;
+//   width: 40px;
+// `
 
-const SmallDate = styled.small`
-  color: ${(p: BaseProps) => p.theme.textTime};
-`
+// const SmallDate = styled.small`
+//   color: ${(p: BaseProps) => p.theme.textTime};
+// `
 
-let initialDate = ``
+// let initialDate = ``
 
-export default (({ channelName, queryMessages: messages }) => {
-  const { username, token } = useAuth()
+export default (({ channelName }) => {
+  const { username } = useAuth()
   const [body, setBody] = useState("")
 
-  client.setHeader("Authorization", `Bearer ${token}`)
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    client
-      .request(CREATE_MESSAGE_MUTATION, {
-        channelName,
-        username,
-        body,
-      })
-      .then(() => {
-        setBody("")
-        revalidate()
-      })
-  }
-
-  const { data, revalidate } = useSwr<QueryMessagesResponse>(
-    [QUERY_MESSAGES_QUERY, channelName],
-    (query, cn) => {
-      return client.request(query, { channelName: cn })
-    }
-  )
-
-  /**
-   * do extra client-side fetching for users.
-   * - message only have username, and no avatarUrl
-   */
-  const [users, dispatch] = useReducer(usersReducer, {})
-  useEffect(() => {
-    ;(data?.queryMessages ?? messages)
-      .reduce((prev, curr) => {
-        if (!prev.includes(curr.username)) {
-          prev.push(curr.username)
-        }
-        return prev
-      }, [] as string[])
-      .map((username) =>
-        client
-          .request<{ getUser: User }>(GET_USER, {
-            username,
-            email: "TODO_THIS_IS_NOT_USED",
-          })
-          .then(({ getUser: u }) => {
-            dispatch({
-              type: UserActionType.SET_USER,
-              username: u.username as string,
-              data: u,
-            })
-          })
-      )
-  }, [data?.queryMessages, messages])
-
-  const { handleSet } = useRightPanel()
-
-  /**
-   * to help generate parent <div>s for sticky date
-   */
-  const grouped = (data?.queryMessages ?? messages)
-    .sort(
-      (a, b) =>
-        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-    )
-    .reduce((group, i) => {
-      // Date Key & Display
-      const date = new Date(i.createdAt)
-
-      const weekday = DAYS[date.getDay()]
-      const month = MONTHS[date.getMonth()]
-      const day = date.getDate()
-
-      const c = `${weekday}, ${month} ${day}${nth(day)}`
-
-      if (Array.isArray(group[c])) {
-        group[c].push(i)
-      } else {
-        group[c] = [i]
-      }
-      return group
-    }, {} as { [date: string]: Message[] })
+  // const { handleSet } = useRightPanel()
 
   return (
     <SlackLayout title={channelName ?? "-"}>
-      {Object.entries(grouped).map(([date, messages], i) => {
-        return (
-          <DateGroup key={`${date}${i}`}>
-            {messages.map((message) => {
-              const { PK, SK, body, username, createdAt } = message
-
-              const buildMessage = (
-                <Message key={`${PK}${SK}`} onClick={() => handleSet(username)}>
-                  <div>
-                    <Img src={users[username]?.avatarUrl}></Img>
-                  </div>
-                  <div>
-                    <b>{username}</b>&nbsp;
-                    <SmallDate>
-                      {new Date(createdAt as string).toLocaleTimeString(
-                        "en-US"
-                      )}
-                    </SmallDate>
-                    <div>{body}</div>
-                  </div>
-                </Message>
-              )
-
-              if (date !== initialDate) {
-                initialDate = date
-                return (
-                  <Fragment key={`${PK}${SK}${date}`}>
-                    <Sticky>
-                      <MessageListDayDivider>
-                        <Day>{date}</Day>
-                      </MessageListDayDivider>
-                    </Sticky>
-                    {buildMessage}
-                  </Fragment>
-                )
-              }
-
-              return buildMessage
-            })}
-          </DateGroup>
-        )
-      })}
-
       {username && (
         <InputContainer>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={() => {}}>
             <input
               value={body}
               onChange={(e) => setBody(e.target.value)}
@@ -312,68 +96,74 @@ export default (({ channelName, queryMessages: messages }) => {
   )
 }) as NextPage<SSRProps>
 
-interface QueryMessagesResponse {
-  queryMessages: Message[]
-}
-interface SSRProps extends QueryMessagesResponse {
+interface SSRProps {
   channelName?: string
 }
 
-export const getServerSideProps: GetServerSideProps<SSRProps> = async ({
-  query,
-}) => {
-  const { teamName, channelName } = query
-
-  const { queryMessages } = await client.request<QueryMessagesResponse>(
-    QUERY_MESSAGES_QUERY,
-    {
-      teamName,
-      channelName,
-    }
-  )
+export const getStaticProps: GetStaticProps<
+  any,
+  {
+    teamName?: string
+    channelName?: string
+  }
+> = async ({ params }) => {
+  const { teamName, channelName } = params ?? {}
 
   return {
     props: {
       teamName: Array.isArray(teamName) ? teamName[0] : teamName,
       channelName: Array.isArray(channelName) ? channelName[0] : channelName,
-      queryMessages,
     },
   }
 }
 
-const MONTHS = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-]
-const DAYS = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-]
-const nth = function (d: number) {
-  if (d > 3 && d < 21) return "th"
-  switch (d % 10) {
-    case 1:
-      return "st"
-    case 2:
-      return "nd"
-    case 3:
-      return "rd"
-    default:
-      return "th"
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          teamName: "test_teamname",
+          channelName: "test_channelName",
+        },
+      }, // See the "paths" section below
+    ],
+    fallback: true || false, // See the "fallback" section below
   }
 }
+
+// const MONTHS = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ]
+// const DAYS = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ]
+// const nth = function (d: number) {
+//   if (d > 3 && d < 21) return "th"
+//   switch (d % 10) {
+//     case 1:
+//       return "st"
+//     case 2:
+//       return "nd"
+//     case 3:
+//       return "rd"
+//     default:
+//       return "th"
+//   }
+// }
--- src/pages/users/[userId]/index.tsx
+++ src/pages/users/[userId]/index.tsx
@@ -1,6 +1,4 @@
-import { NextPage, GetServerSideProps } from "next"
-import Link from "next/link"
-import { request } from "graphql-request"
+import { NextPage } from "next"
 import styled from "styled-components"
 
 import { SlackLayout } from "components/SlackLayout"
@@ -11,83 +9,11 @@ const Card = styled.div`
   display: block;
 `
 
-const ENDPOINT = process.env.ENDPOINT as string
-const GET_USER = `
-fragment Shared on User {
-  PK
-  SK
-  createdAt
-  updatedAt
-  firstName
-  lastName
-  username
-  email
-  avatarUrl
-}
-
-query GetUser($username: String!, $email: String!) {
-  getUser(username: $username, email: $email) {
-    ...Shared
-  }
-}
-`
-
-interface User {
-  PK: string
-  SK: string
-  createdAt?: string
-  updatedAt?: string
-  firstName?: string
-  lastName?: string
-  username?: string
-  email?: string
-  avatarUrl?: string
-}
-
-export default (({ getUser: e }) => {
+const UserPage: NextPage = () => {
   return (
     <SlackLayout title={"User"}>
-      <Card>
-        <h2>
-          <Link href={"/users/[userId]/"} as={`/users/${e.username}`}>
-            <a>{e.username}</a>
-          </Link>
-        </h2>
-        <img src={e.avatarUrl} alt={"User Avatar"} />
-        <section>
-          <div>
-            <code>{e.username}</code>
-          </div>
-          <div>
-            <code>
-              {e.firstName} {e.lastName}
-            </code>
-          </div>
-          <Link
-            href={"/users/[userId]/votes"}
-            as={`/users/${e.username}/votes`}
-          >
-            <a>Votes</a>
-          </Link>
-        </section>
-      </Card>
+      <Card></Card>
     </SlackLayout>
   )
-}) as NextPage<SSRProps>
-
-interface SSRProps {
-  getUser: User
 }
-
-export const getServerSideProps: GetServerSideProps<SSRProps> = async ({
-  query,
-}) => {
-  const { userId } = query
-  const { getUser } = await request<SSRProps>(ENDPOINT, GET_USER, {
-    email: "test",
-    username: userId,
-  })
-  return {
-    props: { getUser },
-  }
-}
+export default UserPage
